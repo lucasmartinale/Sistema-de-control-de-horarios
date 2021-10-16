5b12bfcbd8824d112f84ced50bb32ca9c0a5ca45
Lucas <lucas_m_ale@hotmail.com>
2021-10-16 18:35:32 -0300

    Mejoras varias y comienzo de procesar horarios

--- main.py
+++ main.py
@@ -6,146 +6,208 @@ import pandas as pd
 import tkinter as tk
 from tkinter import ttk
 
+import os
 
-#Crea un excel para cada empleado con sus respectivas entradas y salidas
+
+def procesarHorarioPersona(_nombre_persona):
+    print("-------->procesarHorarioPersona")
+
+    # Busca el path de la carpeta raiz
+    path_excelsPersonas = os.getcwd()
+
+    # Abro el archivo de la persona
+    df = pd.read_excel(path_excelsPersonas +
+                       '\\ExcelsPersonas\\'+_nombre_persona)
+    print("Abrio el archivo de excel de "+_nombre_persona)
+
+    # guardo los nombres en un array
+    lista_cin_cout = list(df['Clock-in/out'])
+
+    # iterar filas
+    # Iteración por filas del DataFrame:
+    for indice_fila, fila in df.iterrows():
+        print(indice_fila)
+	    #print(fila["Hora"])  
+
+        #es un c/in?
+        if(fila["Clock-in/out"] == "C/In"):
+            #El proximo es c/out?
+            print("Es C/IN")
+
+            print(".:Esto es shift:.")
+            print(fila['Clock-in/out'].shift(-1))
+            df['Clock-in/out'].shift().where(df['Letter'].shift() == df['Letter'], '')
+
+            #if(fila['Clock-in/out'].shift(-1) == "C/Out"):
+            #    print("     ES UN C/OUT")
+            #else:
+            #    print("     NO ES UN C/OUT")
+            
+        else:
+            print("Es C/OUT")
+
+
+
+
+def procesarTodosLosHorarios():
+    print("-------->ProcesarTodosLosHorarios")
+    # TODO: Tomar la lista de personas y ponerla en un vector
+
+    # TODO: Hacer un while que tome los excels de las personas
+    # Dentro del while busca el nombre que coincide con el elemento de la lista de personas
+
+    procesarHorarioPersona("Barroso Maria Elena_horarios.xlsx")
+
+
+# Crea un excel para cada empleado con sus respectivas entradas y salidas
 def dividirEmpleadosEnExcels():
-    excelLimpio = pd.read_excel('limpio.xlsx')  
-    listaNombres = pd.read_excel('ListaNombres.xlsx')  
+    print("-------->dividirEmpleadosEnExcels")
 
-    #guardo los nombres en un array
+    excelLimpio = pd.read_excel('limpio.xlsx')
+    listaNombres = pd.read_excel('ListaNombres.xlsx')
+
+    # guardo los nombres en un array
     vector_nombres = list(listaNombres['Name'])
 
-    #recorrer listaNombres
-    for i in vector_nombres :
+    # Crea la carpeta ExcelsPersonas para meter los excels de manera organizada
+    os.makedirs('ExcelsPersonas', exist_ok=True)
+
+    # recorrer listaNombres
+    for i in vector_nombres:
 
         filas_persona = excelLimpio['Name'] == i
         filas_que_cumplen = excelLimpio[filas_persona]
-        filas_que_cumplen.to_excel(i+'_horarios.xlsx', index=False)
 
+        # Busca el path de la carpeta raiz
+        path_excelsPersonas = os.getcwd()
+        # Crea el excel en la carpeta ExcelPersonas que esta dentro de la carpeta raiz
+        filas_que_cumplen.to_excel(
+            path_excelsPersonas+'/ExcelsPersonas/'+i+'_horarios.xlsx', index=False)
 
 
 def definirTurno():
-    excelLimpio = pd.read_excel('limpio.xlsx')  
-    #Se filtran las filas que son C/in (osea entradas)
+    print("-------->definirTurno")
+    excelLimpio = pd.read_excel('limpio.xlsx')
+    # Se filtran las filas que son C/in (osea entradas)
     entradas_cin = excelLimpio['Clock-in/out'] == "C/In"
-    entradas_limpias=excelLimpio[entradas_cin]
-    entradas_limpias.to_excel('soloEntradas.xlsx',index=False)
+    entradas_limpias = excelLimpio[entradas_cin]
+    entradas_limpias.to_excel('soloEntradas.xlsx', index=False)
 
-    
-    excelLimpio = pd.read_excel('soloEntradas.xlsx') 
-    excelLimpio['Turno']="Nada"
+    excelLimpio = pd.read_excel('soloEntradas.xlsx')
+    excelLimpio['Turno'] = "Nada"
 
     for i, fila in excelLimpio.iterrows():
-        #con ese numero determinar el turno
-        #extraemos los dos primeros caracteres
+        # con ese numero determinar el turno
+        # extraemos los dos primeros caracteres
         subcadena = fila['Hora'][0:2]
-        #lo convertimos en numero
-        numero_hora=int(subcadena)
-        #en base al numero determinamos el turno
-        
-        if 20<=numero_hora<=23:
-            excelLimpio.at[i,'Turno']='Nocturno'
-        elif 0<=numero_hora<=3:
-            excelLimpio.at[i,'Turno']='Nocturno'
-        elif 4<=numero_hora<=12:
-            excelLimpio.at[i,'Turno']='Mañana'
-        elif 13<=numero_hora<=19:
-            excelLimpio.at[i,'Turno']='Tarde'
+        # lo convertimos en numero
+        numero_hora = int(subcadena)
+        # en base al numero determinamos el turno
+
+        if 20 <= numero_hora <= 23:
+            excelLimpio.at[i, 'Turno'] = 'Nocturno'
+        elif 0 <= numero_hora <= 3:
+            excelLimpio.at[i, 'Turno'] = 'Nocturno'
+        elif 4 <= numero_hora <= 12:
+            excelLimpio.at[i, 'Turno'] = 'Mañana'
+        elif 13 <= numero_hora <= 19:
+            excelLimpio.at[i, 'Turno'] = 'Tarde'
         else:
             print("Error")
-    excelLimpio.to_excel('soloEntradas.xlsx',index=False)
+    excelLimpio.to_excel('soloEntradas.xlsx', index=False)
+
 
 def limpiezaExcel(df):
-    df=df.drop(columns=['Num', 'Department','Verifycode','Device ID','Device Name','UserExtFmt'])
-    
-    #Extraer fecha y hora
-    df['Fecha']=df['Date/Time'].str.extract('(....-..-..)',expand=True)
-    df['Hora']=df['Date/Time'].str.extract('(..:..:..)',expand=True)
+    print("-------->limpiezaExcel")
+    df = df.drop(columns=['Num', 'Department', 'Verifycode',
+                 'Device ID', 'Device Name', 'UserExtFmt'])
 
-    caja_de_texto.insert(0,"Se ha realizado limpieza de datos")
-    df.to_excel('limpio.xlsx',index=False)
+    # Extraer fecha y hora
+    df['Fecha'] = df['Date/Time'].str.extract('(....-..-..)', expand=True)
+    df['Hora'] = df['Date/Time'].str.extract('(..:..:..)', expand=True)
+
+    caja_de_texto.insert(0, "Se ha realizado limpieza de datos")
+    df.to_excel('limpio.xlsx', index=False)
     return df
-    
-def extraerListaNombres(df):
-    #borrar todas las columnas que no sean nombres
-    df=df.drop(columns=['ID', 'Date/Time','Clock-in/out','Fecha','Hora'])
 
-    #quitar duplicados
+
+def extraerListaNombres(df):
+    print("-------->extraerListaNombres")
+    # borrar todas las columnas que no sean nombres
+    df = df.drop(columns=['ID', 'Date/Time', 'Clock-in/out', 'Fecha', 'Hora'])
+
+    # quitar duplicados
     df = df.drop_duplicates()
 
-    #exportar lista de nombres en un excel
-    df.to_excel('ListaNombres.xlsx',index=False)
-    caja_de_texto.insert(0,"Se ha extraido la lista de personas")
+    # exportar lista de nombres en un excel
+    df.to_excel('ListaNombres.xlsx', index=False)
+    caja_de_texto.insert(0, "Se ha extraido la lista de personas")
+
 
 def abrirArchivo():
-     path_archivo = filedialog.askopenfilename(title="Abrir", initialdir=r"C:\Users\Lucas\Desktop\Practicas comunitarias Don Bosco")
-     print(path_archivo) #en archivo se guarda el path del archivo que quiero abrir
-     df = pd.read_excel(path_archivo)    
-     messagebox.showinfo(message="El archivo ha sido importado", title="Horarios cargados")
+    print("-------->abrirArchivo")
 
-     #Acciones que suceden despues de abrir el libro
-     df= limpiezaExcel(df)  #guarda el limpio en df
-     extraerListaNombres(df)
-     definirTurno()
+    path_archivo = filedialog.askopenfilename(
+        title="Abrir", initialdir=r"C:\Users\Lucas\Desktop\Todo COMUNITARIAS\Practicas comunitarias Don Bosco")
+    # en archivo se guarda el path del archivo que quiero abrir
+    print(path_archivo)
+    df = pd.read_excel(path_archivo)
+    messagebox.showinfo(message="El archivo ha sido importado",
+                        title="Horarios cargados")
+
+    # Acciones que suceden despues de abrir el libro
+    df = limpiezaExcel(df)  # guarda el limpio en df
+    extraerListaNombres(df)
+    # definirTurno()
+    dividirEmpleadosEnExcels()
+    procesarTodosLosHorarios()
+
 
 def cargarListaPersonas():
-    excelNombres = pd.read_excel('ListaNombres.xlsx')  
+    excelNombres = pd.read_excel('ListaNombres.xlsx')
     excelNombres.reset_index(drop=True, inplace=True)
-    nombres=excelNombres["Name"]
-    vlist=[]
+    nombres = excelNombres["Name"]
+    vlist = []
 
     for i in nombres:
         vlist.append(i)
-    
+
     return vlist
 
+
 if __name__ == "__main__":
-    
-    raiz=Tk()
+
+    raiz = Tk()
     raiz.geometry('410x350')
     raiz.title("Sistema de control de horarios - Hogar Don Bosco")
 
-    #Frames
-    frame = Frame(raiz, width=500,height=100)
-    frame.config(bg="deep sky blue")   
+    # Frames
+    frame = Frame(raiz, width=500, height=100)
+    frame.config(bg="deep sky blue")
     frame.pack()
 
-    frame2 = Frame(raiz,width=500,height=100)  
-    frame2.config(bg="gold")   
+    frame2 = Frame(raiz, width=500, height=100)
+    frame2.config(bg="gold")
     frame2.pack()
 
-    frame3 = Frame(raiz,width=500,height=100)  
-    frame3.config(bg="SlateBlue2")   
+    frame3 = Frame(raiz, width=500, height=100)
+    frame3.config(bg="SlateBlue2")
     frame3.pack()
 
-    frame4 = Frame(raiz,width=500,height=450)  
-    frame4.config(bg="SpringGreen2")   
+    frame4 = Frame(raiz, width=500, height=450)
+    frame4.config(bg="SpringGreen2")
     frame4.pack()
-    
-    #Elementos de la ventana
-    label = Label(frame,text="Hogar Don Bosco")
-    label.config(font=("Arial",24)) 
+
+    # Elementos de la ventana
+    label = Label(frame, text="Hogar Don Bosco")
+    label.config(font=("Arial", 24))
     label.pack()
-    
-    #...........................................
+
+    # ...........................................
 
     Button(frame2, text="Abrir archivo", command=abrirArchivo).pack()
-    
+
     caja_de_texto = tk.Listbox(frame4)
-    caja_de_texto.place(x=5, y=25, width=400, height=200)   
+    caja_de_texto.place(x=5, y=25, width=400, height=200)
 
-    # ...................ComboBox....................
-
-    vlist=cargarListaPersonas()
-    
-    Combo = ttk.Combobox(frame3, values = vlist)
-    Combo.config(width=200)
-    Combo.set("Seleccione a un empleado")
-    Combo.pack(padx = 5, pady = 5)
-    
-    
     raiz.mainloop()
-
-
-
-

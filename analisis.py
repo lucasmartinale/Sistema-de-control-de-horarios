0906f1717bfbda4ef7cf0977cc1ad26bbf6135b5
Lucas <lucas_m_ale@hotmail.com>
2021-11-18 02:16:17 -0300

    Mas prolijo, comentado y ordenado

--- analisis.py
+++ analisis.py
@@ -13,7 +13,6 @@ def AnalisisHoras(_nombre_persona):
                        '\\ExcelsPersonas\\'+_nombre_persona+'\\'+_nombre_persona+'_sin_errores.xlsx')
     print("Abrio el archivo de excel de "+_nombre_persona)
 
-    #cant_filas=range(df.shape[0])
 
     #Se crea el dataframe para el analisis
     df_analisis = pd.DataFrame(columns = ['Inicio', 'Fin', 'Total','Minutos restantes','Minutos'])
@@ -23,33 +22,32 @@ def AnalisisHoras(_nombre_persona):
     #Busco la cantidad de horas que hay entre cada par
     for i in range(0,df.shape[0],2):
         
-        print("--------------")
-        #print(i)
+        #Horario de entrada
         entrada=df.loc[i]["Date/Time"]
-        #print(entrada)
-        t1=pd.to_datetime(entrada)
+        entrada=pd.to_datetime(entrada)
         
-
+        #Horario de salida
         salida=df.loc[i+1]["Date/Time"]
-        #print(salida)
-        t2=pd.to_datetime(salida)
+        salida=pd.to_datetime(salida)
 
-        horas_trabajadas_par= t2-t1
+        #Diferencia entre la hora de entrada y la de salida
+        horas_trabajadas_par= salida-entrada
+
+        #Se calculan las horas de esa diferencia
         duration_in_s = horas_trabajadas_par.total_seconds() 
         hours = divmod(duration_in_s, 3600)[0]
 
+        #Se Calculan los minutos de esa diferencia que no cuentan en las horas calculadas en el anterior
         minutes = divmod(duration_in_s, 60)[0]
         minutos_restantes=minutes - (hours*60)
        
-
         #Va a el excel de errores
-        fila_a_insertar = { 'Inicio': t1,
-                            'Fin': t2, 
+        fila_a_insertar = { 'Inicio': entrada,
+                            'Fin': salida, 
                             'Total': hours,
                             'Minutos restantes':minutos_restantes
                             }
 
-                
         #Añade la columna al dataframe de analisis
         df_analisis=df_analisis.append(fila_a_insertar, ignore_index = True)        
 
@@ -68,10 +66,11 @@ def AnalisisHoras(_nombre_persona):
     nuevas_horas = int(total_minutos_validos//60)
     nuevos_minutos= int(total_minutos_validos % 60)
 
+    #A las horas totales se le suman las horas formadas por los minutos que vale la pena contar
     nuevas_horas=nuevas_horas+horas_totales
     print("{horas} horas y {minutos} minutos".format(horas=nuevas_horas,minutos=nuevos_minutos))
 
-    print("Exportando excel")
+    #Exportar excel
     df_analisis.to_excel(path_raiz +'\\ExcelsPersonas\\'+_nombre_persona+'\\'+_nombre_persona+'_analisis.xlsx', index=False)
 
 AnalisisHoras("ALMARAZ ANGELICA")

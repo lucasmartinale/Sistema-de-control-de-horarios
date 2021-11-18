f2482b2d3736b61ce3c0bf9645f147c69f60c92b
Lucas <lucas_m_ale@hotmail.com>
2021-11-18 02:07:42 -0300

    Calcular horas totales terminado

--- analisis.py
+++ analisis.py
@@ -16,7 +16,7 @@ def AnalisisHoras(_nombre_persona):
     #cant_filas=range(df.shape[0])
 
     #Se crea el dataframe para el analisis
-    df_analisis = pd.DataFrame(columns = ['Inicio', 'Fin', 'Total'])
+    df_analisis = pd.DataFrame(columns = ['Inicio', 'Fin', 'Total','Minutos restantes','Minutos'])
     
     horas_trabajadas_totales=0
 
@@ -35,28 +35,43 @@ def AnalisisHoras(_nombre_persona):
         t2=pd.to_datetime(salida)
 
         horas_trabajadas_par= t2-t1
-        print("Par: {horas}".format(horas=horas_trabajadas_par))
+        duration_in_s = horas_trabajadas_par.total_seconds() 
+        hours = divmod(duration_in_s, 3600)[0]
+
+        minutes = divmod(duration_in_s, 60)[0]
+        minutos_restantes=minutes - (hours*60)
+       
 
         #Va a el excel de errores
-        fila_a_insertar = {'Name': df.loc[i]['Name'], 
-                            'Inicio': t1,
+        fila_a_insertar = { 'Inicio': t1,
                             'Fin': t2, 
-                            'Total': horas_trabajadas_par
+                            'Total': hours,
+                            'Minutos restantes':minutos_restantes
                             }
 
                 
         #Añade la columna al dataframe de analisis
         df_analisis=df_analisis.append(fila_a_insertar, ignore_index = True)        
 
-        #Summa las horas para obtener las totales
+        #Suma las horas para obtener las totales
         horas_trabajadas_totales+= int(horas_trabajadas_par.seconds//3600)
         
+    #Horas totales
+    horas_totales = int(df_analisis['Total'].sum())
 
-    print("Horas totales trabajadas: {horas}".format(horas=horas_trabajadas_totales))
+    #Separa los minutos que valen la pena contar de los que no
+    df_analisis.loc[(df_analisis['Minutos restantes'] < 20) , 'Minutos'] = 0  
+    df_analisis.loc[(df_analisis['Minutos restantes'] >= 20) , 'Minutos'] =  df_analisis['Minutos restantes']
 
-    #Exporta excel a la carpeta del empleado
+    #Ahora si, la suma definitiva de horas y minutos
+    total_minutos_validos=df_analisis['Minutos'].sum()
+    nuevas_horas = int(total_minutos_validos//60)
+    nuevos_minutos= int(total_minutos_validos % 60)
+
+    nuevas_horas=nuevas_horas+horas_totales
+    print("{horas} horas y {minutos} minutos".format(horas=nuevas_horas,minutos=nuevos_minutos))
+
     print("Exportando excel")
     df_analisis.to_excel(path_raiz +'\\ExcelsPersonas\\'+_nombre_persona+'\\'+_nombre_persona+'_analisis.xlsx', index=False)
-
 
 AnalisisHoras("ALMARAZ ANGELICA")
